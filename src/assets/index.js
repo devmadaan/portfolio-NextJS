c23df3f7448a596f2f52d75879826334bb6d1bc8
devmadan <dev.madaan@nftically.com>
2023-12-20 20:37:23 +0530

    portfolio

---
+++ src/assets/index.js
@@ -0,0 +1,84 @@
+import logo from "./logo.svg";
+import logo2 from "./logo2.jpeg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import nextjs from "./tech/Nextjs.png";
+import firebase from "./tech/firebase.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import onething from "./company/onething.png";
+import nftically from "./company/nftically.webp";
+import encro from "./company/encro.jpeg";
+
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import indiaai from "./indiaAi.webp";
+import mymoneymantra from "./mymoneymantra.png";
+import posable from "./posable.png";
+import prescinto from "./prescinto.png";
+
+export {
+  indiaai,
+  mymoneymantra,
+  posable,
+  prescinto,
+  logo,
+  logo2,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  nextjs,
+  firebase,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  onething,
+  nftically,
+  encro,
+  carrent,
+  jobit,
+  tripguide,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,300 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  onething,
+  nftically,
+  encro,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  nextjs,
+  firebase,
+  indiaai,
+  mymoneymantra,
+  posable,
+  prescinto,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React.js Developer",
+    icon: mobile,
+  },
+  {
+    title: "Frontend Developer",
+    icon: backend,
+  },
+  {
+    title: "Javascript Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Next JS",
+    icon: nextjs,
+  },
+  {
+    name: "Firebase",
+    icon: firebase,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  // {
+  //   name: "git",
+  //   icon: git,
+  // },
+  {
+    name: "figma",
+    icon: figma,
+  },
+
+];
+
+const experiences = [
+  {
+    title: "Software Developer",
+    company_name: "NFTICALLY",
+    icon: nftically,
+    iconBg: "#ffffff",
+    date: "April 2022 - Nov 2023",
+    points: [
+      "Working as Senior UI Developer At NFTICALLY",
+      "Main technologies using here are React Js, Redux, Typescript, Context, HTML5, CSS3, SCSS, Tailwind CSS , Vanilla Js, Bootstrap, MySQL, PHP, Laravel blade",
+      "NFTICALLY have total of 3 products Nftically marketplace, Comearth world and Kamoto.ai on which I had worked on.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Frontend Developer",
+    company_name: "Onething Design",
+    icon: onething,
+    iconBg: "#ffffff",
+    date: "April 2021 - April 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Last project was for India's one of the largest University, LPU Official Website, built 120 web pages mainly on Jquery, CSS Pre processors & frameworks, AOS Animation, Motion.Js, Did business communication for this Particular Project",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Encrobytes Technologies",
+    icon: encro,
+    iconBg: "#ffffff",
+    date: "Aug 2020 - March 2021",
+    points: [
+      "Developing and maintaining web and mobile applications using PHP and Flutter",
+      "Responsible for the design ,layout and backend connectivity of a website according to a company's specifications and Contribute in all phases of the development lifecycle.",
+      `Worked in a major projects such as: Warehouse management system (Technology: Core PHP), Social media (Technology: Flutter), Recruitment Portal (Technology: Core PHP)`,
+      "Worked in a Team as well as Handled the individual on 25+ Projects ."
+
+    ],
+  },
+  // {
+  //   title: "Full stack Developer",
+  //   company_name: "Meta",
+  //   icon: meta,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2023 - Present",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "MyMoneyMantra",
+    description:
+      "MyMoneyMantra is your one-stop destination to search for the best Home Loans, Personal Loans and Credit Cards in India. Since 1989, more than 50 lakh customers have fulfilled their dreams with us",
+    tags: [
+      {
+        name: "Reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Nextjs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Nodejs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: mymoneymantra,
+    source_code_link: "https://www.mymoneymantra.com/",
+  },
+  {
+    name: "INDIAai",
+    description:
+      "INDIAai is knowledge portal and an ecosystem building initiative. It stands to unite and promote collaborations with various entities in India’s AI ecosystem.",
+    tags: [
+      {
+        name: "Reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: indiaai,
+    source_code_link: "https://indiaai.gov.in/",
+  },
+  {
+    name: "Prescinto",
+    description:
+      "Prescinto will provide its world-class AI-powered APM solution to monitor, analyze, and optimize the performance of recent additions to the renewable portfolio.",
+    tags: [
+      {
+        name: "Javascript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Scss",
+        color: "green-text-gradient",
+      },
+      {
+        name: "PHP",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: prescinto,
+    source_code_link: "https://prescinto.ai/",
+  },
+  {
+    name: "POSable",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "Javascript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "TailwindCSS",
+        color: "green-text-gradient",
+      },
+    
+    ],
+    image: posable,
+    source_code_link: "https://posable.onething.design/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
